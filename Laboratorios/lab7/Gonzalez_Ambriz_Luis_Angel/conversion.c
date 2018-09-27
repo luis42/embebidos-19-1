0c261ed24a74dc2cc2c94b0ff624e8962d4acc38
Luis Angel Gonzalez Ambriz <vivi_luis15@hotmail.com>
2018-09-27 15:06:19 -0500

    Agregar Archivos

--- Laboratorios/lab7/Gonzalez_Ambriz_Luis_Angel/conversion.c
+++ Laboratorios/lab7/Gonzalez_Ambriz_Luis_Angel/conversion.c
@@ -1,34 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "imagen.h"
+#define DIMASK 3
+
 
 unsigned char * RGBtoGray(unsigned char *imagenRGB,uint32_t width,uint32_t height);
 void GraytoRGB(unsigned char *imagenGray,unsigned char *imagenRGB,uint32_t width,uint32_t height);
 void brilloImagen( unsigned char *imagenGray,uint32_t width,uint32_t height);
-
+unsigned char * reservarMemoria(uint32_t width,uint32_t height);
 
 int main()
 {
 	
 
 	bmpInfoHeader info;
-	unsigned char *imagenRGB,*imagenGray;
+	unsigned char *imagenRGB,*imagenGray,*imagenFiltro;
 
 	imagenRGB = abrirBMP("dark_forest3.bmp",&info);
 
 	displayInfo( &info);
 	imagenGray=RGBtoGray(imagenRGB ,info.width,info.height);
-	brilloImagen(imagenGray,info.width,info.height);
-	GraytoRGB(imagenGray , imagenRGB , info.width,info.height);
+//	brilloImagen(imagenGray,info.width,info.height);
+	imagenFiltro=reservarMemoria(info.width,info.height);
+	filtroImagen(imagenGray,info.width,info.height);
+	
+	GraytoRGB(imagenGray , imagenFiltro , info.width , info.height );
 	printf("Guardando\n");
 
 	guardarBMP("hojaGra2.bmp", &info , imagenRGB);
 	free(imagenRGB);
-	free(imagenGray);
+	free(imagenGray); 
 
 
 
 	return 0;
+}
+
+void filtroImagen(unsigned char * imagenGray,unsigned char * imagenFiltro,uint32_t width,uint32_t height)
+{
+
+	register int y,x,ym,xm;
+
+	int indiceGray,conv,indicem;
+	unsigned char mascara[]={
+							1 ,1 ,1,
+							1 ,1 ,1,
+							1 ,1 ,1};
+	for (y = 0; y <= height-DIMASK ; y++)
+	{
+		for (y = 0; y <= height-DIMASK ; y++)
+		{
+			conv=0;
+			indicem=0;
+			for (ym= 0; ym < DIMASK; ym++)
+			{
+
+				for (xm = 0; i < DIMASK; xm++)
+				{
+					indiceGray = ((y + ym) * width + (x + xm ));
+					conv+=imagenGray[indiceGray]*mascara[indicem++];
+
+				}
+				conv=conv/9;
+				
+			}
+			
+		}
+	}
+
+
+}
+
+unsigned char * reservarMemoria(uint32_t width,uint32_t height)
+{
+	unsigned char *imagenGray;
+	imagenGray=(unsigned char*) malloc(width*height*sizeof(unsigned char));
+	if(imagenGray == NULL)
+	{
+		perror("Error al asignar memoria");
+		exit(EXIT_FAILURE);
+	}
+	return imagenGray;
+
 }
 
 void brilloImagen( unsigned char *imagenGray,uint32_t width,uint32_t height)
@@ -46,6 +99,8 @@ void brilloImagen( unsigned char *imagenGray,uint32_t width,uint32_t height)
 
 
 
+
+
 unsigned char * RGBtoGray(unsigned char *imagenRGB,uint32_t width,uint32_t height)
 {
 
@@ -53,7 +108,8 @@ unsigned char * RGBtoGray(unsigned char *imagenRGB,uint32_t width,uint32_t heigh
 	int indiceRGB,indiceGray;
 	unsigned char grayLevel;
 	unsigned char *imagenGray;
-	imagenGray=(unsigned char*) malloc(width*height*sizeof(unsigned char));
+	//imagenGray=(unsigned char*) malloc(width*height*sizeof(unsigned char));
+	imagenGray=reservarMemoria(width,height);
 	if(imagenGray == NULL)
 	{
 		perror("Error al asignar memoria");
